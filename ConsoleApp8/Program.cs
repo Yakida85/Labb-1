9e7bce4bef5f87a9af6c7141898cf70d335341a1
Admin <Admin@LAPTOP-BGCPL6EU>
2022-12-19 10:18:29 +0100

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,68 @@
+﻿// Include namespace system
+using System;
+using System.IO;
+using System.Numerics;
+
+public class omar2
+{
+
+    public static void Main(String[] args)
+    {
+        string getText = omar2.getText();
+        findsubStrings(getText);
+        Console.WriteLine();
+        Console.WriteLine("Total: " + getTotal().ToString());
+    }
+
+    public static BigInteger subResult = 0;
+    
+    private static String getText()
+    {
+        Console.WriteLine("Enter Input:");
+        return Console.ReadLine();
+    }
+    private static void findsubStrings(String input)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+
+            for (int k = i + 1; k < input.Length; k++)
+            {
+                if (char.IsLetter(input[k]))
+                {
+                    break;
+                }
+                else if (input[i] == input[k])
+                {
+                    string subP = input.Substring(i, k + 1 - i);
+                    string startP = input.Substring(0, i);
+                    int räkna = input.Length - k;
+                    string endP = input.Substring(input.Length - räkna + 1);
+                    omar2.addsubstring(subP);
+                    omar2.toString(startP, subP, endP);
+
+                    break;
+                }
+            }
+        }
+    }
+    private static void addsubstring(String subNumbers)
+    {
+        var num = BigInteger.Parse(subNumbers);
+        subResult += num;
+    }
+    private static BigInteger getTotal()
+    {
+        return omar2.subResult;
+    }
+    private static void toString(String start, String subString, String end)
+    {
+        Console.Write(start);
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write(subString);
+        Console.ResetColor();
+        Console.Write(end);
+        Console.WriteLine();
+    }
+
+}
